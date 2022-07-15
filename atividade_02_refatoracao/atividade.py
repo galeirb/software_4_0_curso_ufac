fccbf70000fdfc11e301745b052942ad2d7e1da5
galeirb <galeirb@gmail.com>
2022-07-14 20:08:50 -0500

    refatoracao_realizada

--- atividade_02_refatoracao/atividade.py
+++ atividade_02_refatoracao/atividade.py
@@ -2,57 +2,80 @@ from datetime import datetime, date
 
 
 class Person:
-    def __init__(self, first_name, last_name, birth_date, age=None):
+    def __init__(self, first_name, last_name, birth_date):
+        self.data_format = '%d/%m/%Y'
         self.first_name = first_name
         self.last_name = last_name
-        self.birth_date = birth_date
-        self.age = age
+        self.birth_date = datetime.strptime(birth_date, self.data_format)
+        self.age = self.age_calculator()
 
-def readcsv():
+    def birth_date_view(self):
+        return self.birth_date.strftime(self.data_format)
+
+    def age_calculator(self):
+        today = date.today()
+        return today.year - self.birth_date.year - ((today.month, today.day) <
+                                                    (self.birth_date.month,
+                                                     self.birth_date.day))
+
+
+def read_and_storage_csv():
     # read data for csv file
     file = open('dados_candidatos.csv', 'rb')
     text_file = file.read().decode('utf-8')
     file.close()
-    list = text_file.splitlines()
-    list2 = []
-    # insert data on list2
-    for i in list[1:]:
+    raw_data = text_file.splitlines()
+    # insert data on processed_data
+    for i in raw_data[1:]:
         data = i.split(',')
-        person = Person(data[10].split(' ')[0], data[10].split(' ')[len(data[10].split(' '))-1], datetime.strptime(data[8], '%d/%m/%Y').date())
-        list2.append(person)
+        first_name = data[10].split(' ')[0]
+        last_name = data[10].split(' ')[-1]
+        birth_data = data[8]
 
+        person = Person(first_name, last_name, birth_data)
+        processed_data.append(person)
+
+
+def classify_persons_by_age():
     per_age = {
-        '21-30': [], '31-40': [], '41-50': [], '51-60': [], '61-70': [], '71+': []
+        '21-30': [],
+        '31-40': [],
+        '41-50': [],
+        '51-60': [],
+        '61-70': [],
+        '71+': []
     }
 
-    # age calculator
-    for i in list2:
-        today = date.today()
-        age = today.year - i.birth_date.year - ((today.month, today.day) < (i.birth_date.month, i.birth_date.day))
-        i.age = age
     # classify persons by age
-    for i in list2:
-        if i.age >= 20 and i.age <= 30:
+    for i in processed_data:
+        if i.age in range(21, 31):
             per_age['21-30'].append(i)
-        if i.age >= 31 and i.age <= 40:
+        elif i.age in range(31, 41):
             per_age['31-40'].append(i)
-        if i.age >= 41 and i.age <= 50:
+        elif i.age in range(41, 51):
             per_age['41-50'].append(i)
-        if i.age >= 51 and i.age <= 60:
+        elif i.age in range(51, 61):
             per_age['51-60'].append(i)
-        if i.age >= 61 and i.age <= 70:
+        elif i.age in range(61, 71):
             per_age['61-70'].append(i)
-        if i.age >= 71:
+        elif i.age >= 71:
             per_age['71+'].append(i)
+
     # print first 10 persons
-    print('First 10 rows')
-    for i in list2[:10]:
-        print('Birthdate: %s Age: %s - %s %s' %(i.birth_date.strftime("%d/%m/%Y"), i.age, i.first_name, i.last_name))
+    print_first = 10
+    print('First ' + str(print_first) + ' rows')
+    for i in processed_data[:print_first]:
+        print('Birthdate: %s Age: %s - %s %s' % (i.birth_date_view(), i.age,
+                                                 i.first_name, i.last_name))
+
     # print total per age
     print('Total per age')
     for i in per_age:
-        print(i +' '+ str(len(per_age[i])))
+        print(i + ' ' + str(len(per_age[i])))
 
 
 if __name__ == '__main__':
-    readcsv()
+    processed_data = []
+    read_and_storage_csv()
+    classify_persons_by_age()
+
